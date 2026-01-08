9030f3714fd516f60e1a3ba6a9b53419e41b5e36
JeonSuna <jsa020910@gmail.com>
2026-01-08 10:58:38 +0900

    movie data불러오기

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,22 @@
-export const metadata = {
-  title: "home",
-};
+"use client";
+import { useEffect, useState } from "react";
 
 export default function Partice() {
-  return <h1>Home</h1>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [movie, setMovies] = useState();
+
+  const getMovies = async () => {
+    const res = await fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/movies"
+    );
+    const movieData = await res.json();
+    console.log(movieData);
+    setMovies(movieData);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return <h1>{isLoading ? "loading😂" : JSON.stringify(movie)}</h1>;
 }
